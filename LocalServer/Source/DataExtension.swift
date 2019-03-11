8bc322ab52fce871d573fce148073351f2986265
Diney Bomfim <dineybomfim@gmail.com>
2019-03-12 01:19:16 +0400

    - Implementing compression recursiveness

--- LocalServer/Source/DataExtension.swift
+++ LocalServer/Source/DataExtension.swift
@@ -12,8 +12,30 @@ import Compression
 // MARK: - Extension - Data Compression
 
 extension Data {
+
+// MARK: - Properties
+	
+	static let flags = Int32(COMPRESSION_STREAM_FINALIZE.rawValue)
 	
 // MARK: - Protected Methods
+	
+	private func processStream(_ stream: inout compression_stream,
+							   _ buffer: UnsafeMutablePointer<UInt8>,
+							   _ bufferSize: Int) -> Data? {
+		var result = Data()
+		
+		if compression_stream_process(&stream, Data.flags) == COMPRESSION_STATUS_OK {
+			guard stream.dst_size == 0 else { return nil }
+			result.append(buffer, count: stream.dst_ptr - buffer)
+			stream.dst_ptr = buffer
+			stream.dst_size = bufferSize
+			guard let next = processStream(&stream, buffer, bufferSize) else { return result }
+			return result + next
+		}
+		
+		result.append(buffer, count: stream.dst_ptr - buffer)
+		return result
+	}
 	
 	private func perform(operation: compression_stream_operation,
 						 source: UnsafePointer<UInt8>) -> Data? {
@@ -38,23 +60,7 @@ extension Data {
 		stream.src_ptr  = source
 		stream.src_size = sourceSize
 		
-		var result = Data()
-		let flags = Int32(COMPRESSION_STREAM_FINALIZE.rawValue)
-		
-		while true {
-			switch compression_stream_process(&stream, flags) {
-			case COMPRESSION_STATUS_OK:
-				guard stream.dst_size == 0 else { return nil }
-				result.append(buffer, count: stream.dst_ptr - buffer)
-				stream.dst_ptr = buffer
-				stream.dst_size = bufferSize
-			case COMPRESSION_STATUS_END:
-				result.append(buffer, count: stream.dst_ptr - buffer)
-				return result
-			default:
-				return nil
-			}
-		}
+		return processStream(&stream, buffer, bufferSize)
 	}
 	
 	func deflate() -> Data? {
