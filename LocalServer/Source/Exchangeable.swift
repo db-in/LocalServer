f0284fe3b57a407e7464fd8cc5a92c58f6300bcf
Diney Bomfim <dineybomfim@gmail.com>
2019-03-13 09:35:06 +0400

    - Removing simular code

--- LocalServer/Source/Exchangeable.swift
+++ LocalServer/Source/Exchangeable.swift
@@ -10,6 +10,8 @@ import Foundation
 
 // MARK: - Definitions -
 
+private typealias MethodFunction = (AnyClass?, Selector) -> Method?
+
 protocol Exchangeable : AnyObject {
 	
 	static func exchange()
@@ -19,19 +21,19 @@ protocol Exchangeable : AnyObject {
 
 extension Exchangeable {
 	
-	static func exchangeInstance(_ original: Selector, _ new: Selector) {
-		let originalMethod = class_getInstanceMethod(self, original)
-		let newMethod = class_getInstanceMethod(self, new)
+	static private func exchange(_ method: MethodFunction, _ original: Selector, _ new: Selector) {
+		let originalMethod = method(self, original)
+		let newMethod = method(self, new)
 		if let originalSel = originalMethod, let newSel = newMethod {
 			method_exchangeImplementations(originalSel, newSel)
 		}
 	}
 	
+	static func exchangeInstance(_ original: Selector, _ new: Selector) {
+		exchange(class_getInstanceMethod, original, new)
+	}
+	
 	static func exchangeClass(_ original: Selector, _ new: Selector) {
-		let originalMethod = class_getClassMethod(self, original)
-		let newMethod = class_getClassMethod(self, new)
-		if let originalSel = originalMethod, let newSel = newMethod {
-			method_exchangeImplementations(originalSel, newSel)
-		}
+		exchange(class_getClassMethod, original, new)
 	}
 }
