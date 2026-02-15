5a7e3b9241dcaf48e5214d0d4faa697078caa7a2
Diney Bomfim <926608+dineybomfim@users.noreply.github.com>
2026-02-15 16:04:08 +0400

    - SPM update

--- Pods/Target Support Files/LocalServer/LocalServer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LocalServer : NSObject
-@end
-@implementation PodsDummy_LocalServer
-@end
--- Pods/Target Support Files/Pods-ModuleIntegrationTests/Pods-ModuleIntegrationTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ModuleIntegrationTests : NSObject
-@end
-@implementation PodsDummy_Pods_ModuleIntegrationTests
-@end
